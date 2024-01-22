e449d5798d9952c614bf2d688c933f25f1465ea7
Ruth Oldja <rutholdja@icloud.com>
2024-01-22 08:36:44 -0500

    oauth working

--- app.js
+++ app.js
@@ -3,6 +3,44 @@ const app = express();
 const port = process.env.PORT || 3001;
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
+require('dotenv').config();
+const { google } = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
+
+const createTransporter = async () => {
+  const oauth2Client = new OAuth2(
+    process.env.CLIENT_ID,
+    process.env.CLIENT_SECRET,
+    "https://developers.google.com/oauthplayground"
+  );
+
+  oauth2Client.setCredentials({
+    refresh_token: process.env.REFRESH_TOKEN
+  });
+
+  const accessToken = await new Promise((resolve, reject) => {
+    oauth2Client.getAccessToken((err, token) => {
+      if (err) {
+        reject();
+      }
+      resolve(token);
+    });
+  });
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      type: "OAuth2",
+      user: process.env.EMAIL,
+      accessToken,
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      refreshToken: process.env.REFRESH_TOKEN
+    }
+  });
+
+  return transporter;
+};
 
 const config = {
   service: "gmail",
@@ -15,16 +53,10 @@ const config = {
   },
 };
 
-const send = async (data) => {
-  const transporter = nodemailer.createTransport(config);
-  transporter.sendMail(data, (err, info) => {
-    if (err) {
-      console.log(err);
-    } else {
-      return info.response;
-    }
-  });
-}
+const sendEmail = async (emailOptions) => {
+  let emailTransporter = await createTransporter();
+  await emailTransporter.sendMail(emailOptions);
+};
 
 app.use(express.static('public'));
 app.use(express.static(__dirname + '/views'));
@@ -36,12 +68,12 @@ app.post("/freequote", async (req, res) => {
   const { name, phone, email, address1, address2, city, state, zip, footage, hearAbout, checkAllApply } = req.body;
   console.log(req.body);
   const emailData = {
-    from: email,
+    from: process.env.EMAIL,
     to: "nemucha115@gmail.com", // TODO: make this env var, Zach's business email
     subject: "test",
     text: "test text...",
   };
-  await send(emailData);
+  await sendEmail(emailData);
   res.redirect("/thankyou.html");
 });
 
