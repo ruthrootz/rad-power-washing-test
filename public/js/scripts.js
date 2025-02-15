37d4991b54996a627f62b771f6413e7299d0264c
Ruth Oldja <48135255+ruthrootz@users.noreply.github.com>
2025-02-15 16:22:14 -0500

    Update scripts.js

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -49,3 +49,21 @@ window.addEventListener('DOMContentLoaded', event => {
 
 });
 
+function deRequireCheckbox(elClass) {
+    el = document.getElementsByClassName(elClass);
+    var atLeastOneChecked = false; //at least one cb is checked
+    for (i = 0; i < el.length; i++) {
+        if (el[i].checked === true) {
+            atLeastOneChecked = true;
+        }
+    }
+    if (atLeastOneChecked === true) {
+        for (i = 0; i < el.length; i++) {
+            el[i].required = false;
+        }
+    } else {
+        for (i = 0; i < el.length; i++) {
+            el[i].required = true;
+        }
+    }
+}
